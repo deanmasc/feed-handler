e4644d96ea999704c5c9af3e4d481fa59f64edee
deanmasc <deanmascitti@gmail.com>
2026-06-24 01:26:29 +1000

    Add networking functionality and initial itch reader files
    - Implement setup_socket and send_market_data functions in network.cpp
    - Create itch_reader.cpp and itch_reader.h files for parsing data
    - Update .gitignore to exclude VSCode settings

--- src/exchange/main.cpp
+++ src/exchange/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <network.h>
 
-constexpr std::string MULTICAST_IP_ADDR {"224.0.0.1"};
-constexpr int MULTICAST_PORT {30000};
-
 
 int main() {
     std::cout << "Exchange process started" << std::endl;
 
     // First thing we want to do is create the multicast UDP socket
+    setup_socket();
+
+    // Then we want to call some parse data function, which calls in itch_reader.cpp
+    // this file reads itch binary, wraps in MoldUDP64 header, and calls send_market_data
+    // defined in network.cpp
 
 
     return 0;
--- src/exchange/network.h
+++ src/exchange/network.h
@@ -3,6 +3,11 @@
 #ifndef EXCHANGE_HEADER
 #define EXCHANGE_HEADER
 
+constexpr std::string_view MULTICAST_IP_ADDR {"239.0.0.1"};
+constexpr int MULTICAST_PORT {30000};
 
+void setup_socket();
+void send_market_data(char data[]);
+void close_socket();
 
 #endif
